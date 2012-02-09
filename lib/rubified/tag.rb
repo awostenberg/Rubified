76058a32400e4f9b26745f56aafd70a88690c638
J. Wostenberg <awosty@gmail.com>
2012-02-08 20:04:59 -0700

    You can now create new tag classes easily with Tag::new_tag

--- lib/rubified/tag.rb
+++ lib/rubified/tag.rb
@@ -1,4 +1,20 @@
 class Rubified::Tag
+  # Create and returns a new tag class with the name +name+.
+  def self.new_tag(name)
+    # Store the new tag's name in a constant; the block to create the new class
+    # won't keep local variables. A bit of a cheat
+    const_set(:Name, name)
+    newbie = Class.new(self) { |tclass|
+      def initialize(params)
+        super(params)
+        @tname = self.class.to_s.split("::").last
+      end
+    }
+    remove_const(:Name)
+    const_set(name.capitalize, newbie)
+    newbie
+  end
+
   def initialize(params)
     # The name of the tag, e.g. font
     @tname = "tag"
